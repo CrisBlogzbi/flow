66d784b2bde4f061d813045252c05da38f068b4e
CrisBlogzbi <crisbogdan2001@outlook.com>
2024-02-05 06:04:50 +0100

    Add static root to settings.

--- flow/settings.py
+++ flow/settings.py
@@ -138,6 +138,7 @@ STATIC_URL = '/staticfiles/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles')
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_root')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
